eb9cfabcbb8ee769de7b54b5228f0bed6f7b7ae6
Rullian Rong <naillur.gnor@gmail.com>
2022-01-15 13:52:12 +0800

    created a function that checks if the numbers are non-negative, typeof number then sum of consecutive numbers starting from the first para up to the second para

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
+    if ((firstNum < 0 || secondNum < 0) ||
+        ((typeof(firstNum) != "number") || (typeof(secondNum) != "number"))) {
+        return "ERROR";
+    } else if (firstNum > secondNum) {
+        while (firstNum >= secondNum) {
+            sum += secondNum;
+            secondNum++;
+        }
+    } else {
+        while (firstNum <= secondNum) {
+            sum += firstNum;
+            firstNum++;
+        }
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
