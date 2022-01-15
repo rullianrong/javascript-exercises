a265600485f93bf72f184776c1b932edb6ab783c
Rullian Rong <naillur.gnor@gmail.com>
2022-01-15 10:54:56 +0800

    created function with 2 param string & num and return combined string*num

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatingString = '';
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            repeatingString += string;
+        }
+        return repeatingString;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
