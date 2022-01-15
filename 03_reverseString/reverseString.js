41680f17ca791d512aa115f670bec1b21e0bbafc
Rullian Rong <naillur.gnor@gmail.com>
2022-01-15 11:12:07 +0800

    created a function that returns a reversed string of the param

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
-
+    let reversedStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str.charAt(i);
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
