e8042f9ae514d067f43f08ff345a0fa7dd7f74b7
Rullian Rong <naillur.gnor@gmail.com>
2022-01-24 20:53:31 +0800

    completed functions for problem set 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  arr.forEach( e => sum += e );
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let product = 1;
+  arr.forEach( e => product *= e );
+  return product;
 };
 
-const power = function() {
-	
+const power = function (x, pow) {
+  return x ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+	let total;
+  return (n == 0 ? 1 : total = n * factorial(n - 1));
 };
 
 // Do not edit below this line
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
