3c7504b325f06d5e98291b713e782949a293fb37
Rullian Rong <naillur.gnor@gmail.com>
2022-01-25 15:06:32 +0800

    answered exercise 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (objArr) {
+  let age = 0;
+  let oldest = {};
 
+  objArr.forEach((element) => {
+    if (element.yearOfDeath == undefined) {
+      const date = new Date();
+      element.yearOfDeath = date.getFullYear();
+    }
+    let yearLiving = element.yearOfDeath - element.yearOfBirth;
+    if (yearLiving > age) {
+      age = yearLiving;
+      oldest = element;
+    }
+  });
+  return oldest;
 };
 
 // Do not edit below this line
