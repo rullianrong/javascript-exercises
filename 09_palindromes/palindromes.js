fcf21243b1959951d6dacd1ebfeaca7f1c642029
Rullian Rong <naillur.gnor@gmail.com>
2022-01-25 12:59:37 +0800

    answered exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let mainString = string.toLowerCase().replace(/[^A-Za-z0–9]/g, "");
+  let mainStringArr = mainString.split("");
+  let finalString = mainStringArr
+    .reverse()
+    .join()
+    .replace(/[^A-Za-z0–9]/g, "");
 
+  return mainString == finalString ? true : false;
 };
 
 // Do not edit below this line
