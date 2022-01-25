f3c0c3941c56f62af4a31060dfa0ac0b9b849e82
Rullian Rong <naillur.gnor@gmail.com>
2022-01-25 13:02:35 +0800

    modify answer to 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,6 @@
 const palindromes = function (string) {
   let mainString = string.toLowerCase().replace(/[^A-Za-z0–9]/g, "");
-  let mainStringArr = mainString.split("");
-  let finalString = mainStringArr
-    .reverse()
-    .join()
-    .replace(/[^A-Za-z0–9]/g, "");
-
+  let finalString = mainString.split('').reverse().join('');
   return mainString == finalString ? true : false;
 };
 
