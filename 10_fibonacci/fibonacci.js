8debcf0073c5f347f68630fd9b6e344c3186dfd1
Rullian Rong <naillur.gnor@gmail.com>
2022-01-25 13:50:10 +0800

    answered exercise 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  let n1 = 0,
+    n2 = 1,
+    nextNum;
+  let itr = 0;
+  if (n >= 0) {
+    while (itr < n) {
+      nextNum = n1 + n2;
+      n1 = n2;
+      n2 = nextNum;
+      itr++;
+    }
+  } else return 'OOPS';
+  return n1;
 };
 
 // Do not edit below this line
